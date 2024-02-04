c34f169b089c036ac8d4d1b7a6e8ec36393111ae
jusish <ishimwejustin67@gmail.com>
2024-02-04 20:49:59 +0200

    new changes

--- server/server.js
+++ server/server.js
@@ -3,5 +3,6 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const todoRoutes = require("./routes/todoRoutes");
-
+require("dotenv"), config();
 const app = express();
+const PORT = process.env.PORT || 8000;
