a8cb0827443b04b477bb6adde64db8f0d1ac6a1d
jusish <ishimwejustin67@gmail.com>
2024-02-05 16:09:07 +0200

    added other operations

--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -2,13 +2,18 @@ const express = require("express");
 const router = express.Router();
 const todoController = require("../controllers/todoController");
 
-// Define routes
 router.get("/todos", todoController.getAllTodos);
+
 router.get("/todos/:id", todoController.getTodoById);
+
+router.get("/todos/search/:title", todoController.getTodosByTitle);
+
 router.post("/todos", todoController.addTodo);
+
 router.put("/todos/:id", todoController.updateTodo);
+
 router.delete("/todos/:id", todoController.deleteTodo);
+
 router.put("/todos/:id/completed", todoController.markTodoAsCompleted);
 
-// Export the router
 module.exports = router;
