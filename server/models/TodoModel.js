be0405cd4c801d2457bcac45b93a2d3eaaf642ea
jusish <ishimwejustin67@gmail.com>
2024-02-05 11:46:39 +0200

    changed schemma

--- server/models/TodoModel.js
+++ server/models/TodoModel.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
-  title: String,
-  startDate: Date,
-  endDate: Date,
+  title: {
+    type: "String",
+    required: true,
+  },
+  startDate: {
+    type: "Date",
+    required: true,
+  },
+  endDate: {
+    type: "Date",
+    required: true,
+  },
+  completed: {
+    type: "Boolean",
+    default: false,
+  },
 });
 
 const Todo = mongoose.model("Todo", todoSchema);
