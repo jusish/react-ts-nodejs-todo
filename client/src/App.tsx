614a348b0daaad8c69607843a6b57a6f106ea0a9
jusish <ishimwejustin67@gmail.com>
2024-02-05 21:14:10 +0200

    modified app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getTodos } from "./store/todo/actions";
+import React from "react";
+// import { useEffect } from 'react';
+// import { useDispatch } from "react-redux";
+// import { getTodos } from './store/todo/actions';
 
 const App: React.FC = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getTodos());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getTodos());
+  // }, [dispatch]);
 
-  return <div>This is the component</div>;
+  return <div className="App">{/* Your components go here */}</div>;
 };
-
 
 export default App;
