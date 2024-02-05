31c2691b2874619e1c0207d9436821ba6ac80a23
jusish <ishimwejustin67@gmail.com>
2024-02-05 21:14:58 +0200

    added list and filter

---
+++ client/src/components/TodoList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { useSelector } from 'react-redux';
+// import { RootState } from '../store/rootReducer';
+
+const TodoList: React.FC = () => {
+//   const todos = useSelector((state: RootState) => state.todo.todos);
+
+  // Handle rendering todos and implement filter logic here
+
+  return (
+    <div>
+      {/* Render your todos and filter options */}
+    </div>
+  );
+};
+
+export default TodoList;
