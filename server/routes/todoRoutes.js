afc8f7b760d5c62814f47f943b1065cb3a6551fe
jusish <ishimwejustin67@gmail.com>
2024-02-05 11:36:24 +0200

    modified the routes

--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const todoController = require('../controllers/todoController');
+const todoController = require("../controllers/todoController");
 
 // Define routes
-router.get('/todos', todoController.getAllTodos);
-router.post('/todos', todoController.addTodo);
-router.put('/todos/:id', todoController.updateTodo);
-router.delete('/todos/:id', todoController.deleteTodo);
+router.get("/todos", todoController.getAllTodos);
+router.get("/todos/:id", todoController.getTodoById);
+router.post("/todos", todoController.addTodo);
+router.put("/todos/:id", todoController.updateTodo);
+router.delete("/todos/:id", todoController.deleteTodo);
 
 // Export the router
 module.exports = router;
