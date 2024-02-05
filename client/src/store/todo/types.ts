80786bcbb9a82fa33273f9f4ecad7669bc4624c5
jusish <ishimwejustin67@gmail.com>
2024-02-05 20:47:08 +0200

    types defined

---
+++ client/src/store/todo/types.ts
@@ -0,0 +1,49 @@
+export const GET_TODOS = "GET_TODOS";
+export const ADD_TODO = "ADD_TODO";
+export const DELETE_TODO = "DELETE _TODO";
+export const UPDATE_TODO = "UPDATE_TODO";
+export const MARK_COMPLETED = "MARK_COMPLETED";
+
+export interface Todo {
+  _id: string;
+  title: string;
+  startDate: Date;
+  endDate: Date;
+  completed: boolean;
+}
+
+export interface TodoState {
+  todos: Todo[];
+}
+
+interface GetTodosAction {
+  type: typeof GET_TODOS;
+  payload: Todo[];
+}
+
+interface AddTodosAction {
+  type: typeof ADD_TODO;
+  payload: Todo;
+}
+
+interface DeleteTodosAction {
+  type: typeof DELETE_TODO;
+  payload: string;
+}
+
+interface UpdateTodosAction {
+  type: typeof UPDATE_TODO;
+  payload: Todo;
+}
+
+interface MarkCompletedAction {
+  type: typeof MARK_COMPLETED;
+  payload: string;
+}
+
+export type TodoActionTypes =
+  | GetTodosAction
+  | AddTodosAction
+  | DeleteTodosAction
+  | UpdateTodosAction
+  | MarkCompletedAction;
