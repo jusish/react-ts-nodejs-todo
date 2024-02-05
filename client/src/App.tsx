6bcbdd798198846744e158ff59605ddff7238027
jusish <ishimwejustin67@gmail.com>
2024-02-05 20:45:41 +0200

    components together

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <div></div>;
-}
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getTodos } from "./store/todo/actions";
+
+const App: React.FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTodos());
+  }, [dispatch]);
+
+  return <div>This is the component</div>;
+};
+
 
 export default App;
