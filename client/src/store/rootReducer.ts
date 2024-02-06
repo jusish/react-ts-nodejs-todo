d0a62fcf0736cab0cb7e7524416e036fb89aba13
jusish <ishimwejustin67@gmail.com>
2024-02-06 21:37:26 +0200

    modified the root reducer

--- client/src/store/rootReducer.ts
+++ client/src/store/rootReducer.ts
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux';
-import todoReducer from './todo/reducers';
+import { combineReducers } from "redux";
+import todoReducer from "./todo/reducers";
 
 const rootReducer = combineReducers({
   todo: todoReducer,
