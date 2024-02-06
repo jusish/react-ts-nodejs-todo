94c50916beb484aed2724f6673a14cf952b856c0
jusish <ishimwejustin67@gmail.com>
2024-02-06 14:47:54 +0200

    corrected types

--- client/src/store/todo/types.ts
+++ client/src/store/todo/types.ts
@@ -1,6 +1,6 @@
 export const GET_TODOS = "GET_TODOS";
 export const ADD_TODO = "ADD_TODO";
-export const DELETE_TODO = "DELETE _TODO";
+export const DELETE_TODO = "DELETE_TODO";
 export const UPDATE_TODO = "UPDATE_TODO";
 export const MARK_COMPLETED = "MARK_COMPLETED";
 
