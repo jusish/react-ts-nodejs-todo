4c5cc1116ca93723df2441780259e3a9c16d7aed
jusish <ishimwejustin67@gmail.com>
2024-02-06 21:22:36 +0200

    removed details

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,7 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getTodos } from "./store/todo/actions";
-import { AppDispatch } from "./store/todo/types"; // Assuming your types file path is correct
+import React from "react";
 
 const App: React.FC = () => {
-  const dispatch: AppDispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getTodos());
-  }, [dispatch]);
-
-  return <div className="App">{/* Your components go here */}</div>;
+  return <div className="App">Todo</div>;
 };
 
 export default App;
