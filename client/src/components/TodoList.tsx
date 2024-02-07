57278bf087cc6b611e9db99da2c8a7b8d19eed82
jusish <ishimwejustin67@gmail.com>
2024-02-07 21:34:12 +0200

    styling components

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -2,8 +2,8 @@ import TodoItem from "./TodoItem";
 
 const TodoList = () => {
   return (
-    <div>
-      <h1>Available todos</h1>
+    <div className="  justify-center flex items-center">
+      <h1 className=" text-red-500">Available todos</h1>
       <div>
         <TodoItem />
       </div>
