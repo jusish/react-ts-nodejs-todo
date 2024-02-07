b9e12765c0a4190a9000527019dc2012e5c78d26
jusish <ishimwejustin67@gmail.com>
2024-02-07 21:19:58 +0200

    creating the todo list

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -1,7 +1,12 @@
+import TodoItem from "./TodoItem";
+
 const TodoList = () => {
   return (
     <div>
       <h1>Available todos</h1>
+      <div>
+        <TodoItem />
+      </div>
     </div>
   );
 };
