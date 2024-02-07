d1f4b1eb28841b29e408895c588cfe948d46590b
jusish <ishimwejustin67@gmail.com>
2024-02-07 21:53:08 +0200

    used tailwind

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -3,7 +3,7 @@ import TodoItem from "./TodoItem";
 const TodoList = () => {
   return (
     <div className="  justify-center flex items-center">
-      <h1 className=" text-red-500">Available todos</h1>
+      <h1 className=" text-red-200">Available todos</h1>
       <div>
         <TodoItem />
       </div>
