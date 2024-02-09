703009bbb0b153dee105ec5321d4031093aa9276
jusish <ishimwejustin67@gmail.com>
2024-02-09 13:27:55 +0200

    created todo item

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -1,5 +1,49 @@
-const TodoItem = () => {
-  return <div>Todo</div>;
+// TodoItems.tsx
+import React from "react";
+import { Todo } from "../store/todo/types";
+import { useDispatch } from "react-redux";
+import { deleteTodo, markCompleted } from "../state/todoSlice";
+
+interface TodoItemProps {
+  todo: Todo;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
+  const dispatch = useDispatch();
+
+  const handleCompleteClick = () => {
+    dispatch(markCompleted(todo._id));
+  };
+
+  const handleDeleteClick = () => {
+    dispatch(deleteTodo(todo._id));
+  };
+
+  return (
+    <div className="flex items-center justify-between px-4 py-2 border-b-2">
+      <div>
+        <h3 className={todo.completed ? "line-through" : ""}>{todo.title}</h3>
+        <p>Start Date: {todo.startDate.toLocaleDateString()}</p>
+        <p>End Date: {todo.endDate.toLocaleDateString()}</p>
+      </div>
+      <div>
+        {!todo.completed && (
+          <button
+            className="px-3 py-1 mr-2 text-white bg-green-500 rounded"
+            onClick={handleCompleteClick}
+          >
+            Complete
+          </button>
+        )}
+        <button
+          className="px-3 py-1 text-white bg-red-500 rounded"
+          onClick={handleDeleteClick}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoItem;
