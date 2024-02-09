67d313808091358f2b93efbd3fa8ccb15b7d1f77
jusish <ishimwejustin67@gmail.com>
2024-02-09 13:29:39 +0200

    wrapped app into provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import "./output.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "./output.css";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./state/todoSlice.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const store = configureStore({
+  reducer: todoReducer,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
