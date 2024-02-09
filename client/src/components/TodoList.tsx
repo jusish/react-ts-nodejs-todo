8dff6657dc377112dd6fc1462a464c2fa70fc075
jusish <ishimwejustin67@gmail.com>
2024-02-09 21:50:35 +0200

    modified list

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -1,18 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // TodoList.tsx
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import TodoItem from "./TodoItem";
-import { TodoState } from "@/store/todo/types";
+import { GET_TODOS } from "@/store/todo/types";
+import axios from "axios";
 
 const TodoList: React.FC = () => {
-  
-  const todos = useSelector((state : TodoState) => state.todos);
+  const dispatch = useDispatch();
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchOnRender = async () => {
+      const res = await axios.get("http://localhost:5000/todos");
+      console.log(res.data);
+
+      dispatch({
+        type: GET_TODOS,
+        payload: res.data,
+      });
+
+      setTodos(res.data);
+    };
+    fetchOnRender();
+  }, []);
 
   return (
     <div>
       <h2 className="mb-4 text-xl font-bold">Todo List</h2>
       <div>
-        {todos.map((todo) => (
+        {todos?.map((todo: any) => (
           <TodoItem key={todo._id} todo={todo} />
         ))}
       </div>
