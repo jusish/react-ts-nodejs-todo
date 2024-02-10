17452f464b636a92b7782a9ad1e715eae22cf5f7
jusish <ishimwejustin67@gmail.com>
2024-02-10 18:17:32 +0200

    Modified items

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -20,23 +20,44 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
   };
 
   return (
-    <div className="mb-10 bg-slate-900 max-w-[500px] p-10 rounded-lg">
-      <div className="">
-        <h3 className={todo.completed ? "line-through" : ""}>{todo.title}</h3>
-        <p>Start Date: {todo.startDate.toString()}</p>
-        <p>End Date: {todo.endDate.toString()}</p>
+    <div
+      className={
+        todo.completed
+          ? " hidden text-gray-50  mb-10 bg-slate-900 max-w-[500px] p-10 rounded-lg"
+          : " text-gray-50  mb-10 bg-slate-900 max-w-[500px] p-10 rounded-lg"
+      }
+    >
+      <div className="gap-10">
+        <h3
+          className={
+            todo.completed
+              ? " text-white line-through mb-5"
+              : " text-gray-50 mb-5"
+          }
+        >
+          {todo.title}
+        </h3>
+        <div className="mb-5 text-gray-300">
+          <p>Start Date: {todo.startDate.toString()}</p>
+          <p>End Date: {todo.endDate.toString()}</p>
+        </div>
       </div>
       <div>
         {!todo.completed && (
-          <button
-            className="px-3 py-1 mr-2 text-white bg-green-500 rounded"
-            onClick={handleCompleteClick}
-          >
-            Complete
-          </button>
+          <div>
+            <button
+              className="px-3 py-1 m-2 text-white bg-green-500 rounded"
+              onClick={handleCompleteClick}
+            >
+              Complete
+            </button>
+            <button className="px-3 py-1 m-2 text-white bg-blue-500 rounded">
+              edit
+            </button>
+          </div>
         )}
         <button
-          className="px-3 py-1 text-white bg-red-500 rounded"
+          className="px-3 py-1 m-2 text-white bg-red-500 rounded"
           onClick={handleDeleteClick}
         >
           Delete
