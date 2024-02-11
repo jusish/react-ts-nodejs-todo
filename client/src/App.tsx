4f1a53e588cb41ced3e9109d6c00dd143f24f1ff
jusish <ishimwejustin67@gmail.com>
2024-02-11 23:03:46 +0200

    modified app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
+import EditForm from "./components/EditForm";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" Component={TodoList} />
         <Route path="/add" Component={TodoForm} />
+        <Route path="/edit/:id" Component={EditForm} />
       </Routes>
     </Router>
   );
