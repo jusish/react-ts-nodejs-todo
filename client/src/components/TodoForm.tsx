c95377c9070a56e8f941cd4519d3a1b4c077652a
jusish <ishimwejustin67@gmail.com>
2024-02-11 21:38:21 +0200

    modified form

--- client/src/components/TodoForm.tsx
+++ client/src/components/TodoForm.tsx
@@ -11,6 +11,7 @@ const TodoForm: React.FC = () => {
     endDate: "",
   });
 
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
@@ -19,42 +20,52 @@ const TodoForm: React.FC = () => {
     e.preventDefault();
 
     dispatch(addTodo(formData));
-    
+
     setFormData({
       title: "",
       startDate: "",
       endDate: "",
     });
-  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="title"
-        value={formData.title}
-        onChange={handleChange}
-        placeholder="Title"
-        required
-      />
-      <input
-        type="date"
-        name="startDate"
-        value={formData.startDate}
-        onChange={handleChange}
-        placeholder="Start Date"
-        required
-      />
-      <input
-        type="date"
-        name="endDate"
-        value={formData.endDate}
-        onChange={handleChange}
-        placeholder="End Date"
-        required
-      />
-      <button type="submit">Add Todo</button>
-    </form>
+    <div className="flex flex-col max-w-[500px] items-center justify-center">
+      <p className="items-center text-2xl text-blue-500">Add Todo</p>
+      <form onSubmit={handleSubmit} className="flex flex-col ">
+        <input
+          type="text"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+          placeholder="Title"
+          required
+          className="p-2 m-2 border border-solid rounded-sm border-gray"
+        />
+        <input
+          type="date"
+          name="startDate"
+          value={formData.startDate}
+          onChange={handleChange}
+          placeholder="Start Date"
+          required
+          className="p-2 m-2 border border-solid rounded-sm border-gray"
+        />
+        <input
+          type="date"
+          name="endDate"
+          value={formData.endDate}
+          onChange={handleChange}
+          placeholder="End Date"
+          required
+          className="p-2 m-2 border border-solid border-gray"
+        />
+        <button
+          type="submit"
+          className="px-3 py-1 m-2 text-white bg-blue-500 rounded"
+        >
+          Add Todo
+        </button>
+      </form>
+    </div>
   );
 };
 
